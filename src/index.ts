d64fcaded19e084172b6ad6b12b9e3ec5788cabe
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-01-31 22:11:22 +0100

    Add toHaveBeenRenderedWith

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,17 @@ import MatcherContext = jest.MatcherContext;
 // eslint-disable-next-line no-use-before-define
 import CustomMatcherResult = jest.CustomMatcherResult;
 
-interface ReactMockExpect {
+type DeepPartial<T> = T extends object
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : T;
+
+interface ReactMockExpect<P> {
   toHaveBeenRendered(): void;
+  toHaveBeenRenderedWith(props: DeepPartial<P>): void;
 }
 
 type ReactMockMatcher = {
-  [P in keyof ReactMockExpect]: ReactMockExpect[P] extends (
+  [P in keyof ReactMockExpect<any>]: ReactMockExpect<any>[P] extends (
     ...args: infer A
   ) => void
     ? (
@@ -23,9 +28,34 @@ type ReactMockMatcher = {
 declare global {
   namespace jest {
     interface Expect {
-      (mock: ReactMock<any>): ReactMockExpect & { not: ReactMockExpect };
+      <P>(mock: ReactMock<P>): ReactMockExpect<P> & { not: ReactMockExpect<P> };
     }
   }
+}
+
+function getMatchingCalls<P>(
+  mock: ReactMock<P>,
+  expected: P,
+  printReceived: (object: any) => string
+) {
+  if (!mock.renderedWith(expected)) {
+    return '';
+  }
+  const matchingCalls: [P, number][] = [];
+
+  mock.renderCalls.forEach((props, i) => {
+    try {
+      // expect in expect, yeah.
+      expect(props).toMatchObject(expected);
+
+      matchingCalls.push([props, i]);
+      // eslint-disable-next-line no-empty
+    } catch (e) {}
+  });
+
+  return matchingCalls
+    .map(([props, i]) => `  ${i}: ${printReceived(props)}`)
+    .join('\n');
 }
 
 const reactMockMatcher: ReactMockMatcher = {
@@ -48,6 +78,38 @@ const reactMockMatcher: ReactMockMatcher = {
 Expected number of renders: ${expected}
 Received number of renders: ${received}`,
       pass: mock.rendered
+    };
+  },
+
+  toHaveBeenRenderedWith<P>(
+    this: MatcherContext,
+    mock: ReactMock<P>,
+    props: P
+  ) {
+    const { isNot } = this;
+    const { printExpected, printReceived, matcherHint } = this.utils;
+
+    const hint = matcherHint('toHaveBeenRenderedWith', `mock`, 'props', {
+      isNot
+    });
+
+    const expected = printExpected(props);
+    const received = isNot
+      ? getMatchingCalls(mock, props, printReceived)
+      : mock.renderCalls
+          .map((receivedProps, i) => `  ${i}: ${printReceived(receivedProps)}`)
+          .join('\n');
+
+    return {
+      message: () =>
+        `${hint}
+
+Expected: ${isNot ? 'not ' : ''}${expected}
+Received:
+${received}
+
+Number of renders: ${mock.renderCalls.length}`,
+      pass: mock.renderedWith(props)
     };
   }
 };
--- tests/index.spec.tsx
+++ tests/index.spec.tsx
@@ -15,7 +15,7 @@ function expectToThrowAnsiless(cb: () => void, message: string) {
 describe('jest-react-mock', () => {
   expect.extend(reactMockMatcher);
 
-  it('should check that a component was rendered', () => {
+  it('should check that a mock was rendered', () => {
     const Mock = createReactMock();
 
     expectToThrowAnsiless(
@@ -37,5 +37,83 @@ Expected number of renders: 0
 Received number of renders: 1`
     );
     expect(Mock).toHaveBeenRendered();
+  });
+
+  it('should check that a mock was rendered with certain props', () => {
+    const Mock = createReactMock<{ foo: string }>();
+
+    $render(<Mock foo="bar" />);
+    $render(<Mock foo="baz" />);
+
+    expectToThrowAnsiless(
+      () => expect(Mock).toHaveBeenRenderedWith({ foo: 'no' }),
+      `expect(mock).toHaveBeenRenderedWith(props)
+
+Expected: {"foo": "no"}
+Received:
+  0: {"foo": "bar"}
+  1: {"foo": "baz"}
+
+Number of renders: 2`
+    );
+
+    expect(Mock).not.toHaveBeenRenderedWith({ foo: 'no' });
+    expect(Mock).toHaveBeenRenderedWith({ foo: 'bar' });
+    expect(Mock).toHaveBeenRenderedWith({ foo: 'baz' });
+  });
+
+  it('should check that a mock was rendered with partial props', () => {
+    const Mock = createReactMock<{ foo: string; bar: number }>();
+
+    $render(<Mock foo="bar" bar={23} />);
+    $render(<Mock foo="baz" bar={42} />);
+
+    expectToThrowAnsiless(
+      () => expect(Mock).toHaveBeenRenderedWith({ foo: 'no' }),
+      `expect(mock).toHaveBeenRenderedWith(props)
+
+Expected: {"foo": "no"}
+Received:
+  0: {"bar": 23, "foo": "bar"}
+  1: {"bar": 42, "foo": "baz"}
+
+Number of renders: 2`
+    );
+
+    expect(Mock).toHaveBeenRenderedWith({ foo: 'bar' });
+    expect(Mock).toHaveBeenRenderedWith({ bar: 23 });
+    expect(Mock).toHaveBeenRenderedWith({});
+  });
+
+  it('should check that a mock was rendered with partial nested props', () => {
+    const Mock = createReactMock<{ foo: { bar: number; baz: boolean } }>();
+
+    $render(<Mock foo={{ bar: 23, baz: true }} />);
+
+    expect(Mock).toHaveBeenRenderedWith({ foo: { bar: 23 } });
+    expect(Mock).toHaveBeenRenderedWith({ foo: { baz: true } });
+  });
+
+  it('should print all props that match for failed negated expectation', () => {
+    const Mock = createReactMock<{ foo: string; bar: number }>();
+
+    $render(<Mock foo="bar" bar={23} />);
+    $render(<Mock foo="baz" bar={42} />);
+    $render(<Mock foo="baz" bar={43} />);
+
+    expectToThrowAnsiless(
+      () => expect(Mock).not.toHaveBeenRenderedWith({ foo: 'baz' }),
+      `expect(mock).not.toHaveBeenRenderedWith(props)
+
+Expected: not {"foo": "baz"}
+Received:
+  1: {"bar": 42, "foo": "baz"}
+  2: {"bar": 43, "foo": "baz"}
+
+Number of renders: 3`
+    );
+    expect(Mock).toHaveBeenRenderedWith({ foo: 'bar' });
+    expect(Mock).toHaveBeenRenderedWith({ bar: 23 });
+    expect(Mock).toHaveBeenRenderedWith({});
   });
 });
